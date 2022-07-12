1cff49155752d7ff1b96d9ffd7a16dc6727f99c8
Emanuelle Costa <emanuellemcosta@outlook.com>
2022-07-12 10:48:38 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmeta/ProjetoDaSemanaSpringReactDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDaSemanaSpringReactDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDaSemanaSpringReactDevSuperiorApplication.class, args);
+	}
+
+}
